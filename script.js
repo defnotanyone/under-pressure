024bacb8b6f6cc6947399bc610b328531b8e8c51
defnotanyone <140023334+defnotanyone@users.noreply.github.com>
2025-05-02 21:50:42 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Function to scroll to different sections of the page
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
